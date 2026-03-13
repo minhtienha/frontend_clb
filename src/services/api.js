36eb008c39950d0ec32e9cf8be6d44e43098459d
Hà Minh Tiến <minhtien250704@gmail.com>
2026-03-13 22:57:57 +0700

    update link backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://backend-clb.onrender.com/api",
 });
 
 export const getHomeData = () => api.get("/home");
